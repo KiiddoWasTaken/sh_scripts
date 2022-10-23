f7aca2711b238ac776092cb57071ccee09487e85
KiiddoWasTaken <115580874+KiiddoWasTaken@users.noreply.github.com>
2022-10-23 23:18:59 +0200

    Update docker_mysql_backup.sh

--- docker_mysql_backup.sh
+++ docker_mysql_backup.sh
@@ -7,7 +7,7 @@ user="root"
 passwd=".123456aA."
 container="wpdb"
 db="mysql_db_sds"
-days="+10
+days="+10"
 
 # Getting in the mysql container and dumping out file with date to host 
 docker exec -i $container mysqldump -u $user --password=$passwd $db > $dir$(date +"%Y-%m-%d-%H-%M-%S").sql
