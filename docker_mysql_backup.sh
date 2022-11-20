bd1352742af62a65c1f802c4dee83af198412687
KiiddoWasTaken <115580874+KiiddoWasTaken@users.noreply.github.com>
2022-11-20 22:47:21 +0100

    Update docker_mysql_backup.sh

--- docker_mysql_backup.sh
+++ docker_mysql_backup.sh
@@ -9,9 +9,9 @@ container="wpdb"
 db="mysql_db_sds"
 days="+10"
 
-# Getting in the mysql container and dumping out file with date to host 
+# Getting in the mysql container and dumping out file with date to host
 docker exec -i $container mysqldump -u $user --password=$passwd $db > $dir$(date +"%Y-%m-%d-%H-%M-%S").sql
 # Deletes backups from x days old
-find $dir -type f -mtime $days -delete
+find $dir -type f -ctime $days -delete
 
 echo "mysql_db_sds backup complete."
