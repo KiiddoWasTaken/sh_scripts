1ec13ce49afc023689d75723d67f84f685219fb0
KiiddoWasTaken <115580874+KiiddoWasTaken@users.noreply.github.com>
2022-11-20 22:48:57 +0100

    Update docker_volumes_backup.sh

--- docker_volumes_backup.sh
+++ docker_volumes_backup.sh
@@ -3,10 +3,12 @@
 # Add file to crontab for regular backups
 
 dir="/backups/backup_volumes/"
-target_dir="/volums"
+target_dir="/volums/"
 days="+10"
 
+docker stop $(docker ps -a -q)
 cp -rp $target_dir $dir$(date +"%Y-%m-%d-%H-%M-%S")
-find $dir* -mtime $days -exec rm -rf {} \;
+find $dir* -type d -ctime $days -exec rm -rf {} \;
+docker start $(docker ps -a -q)
 
 echo "docker volumes backup complete"
